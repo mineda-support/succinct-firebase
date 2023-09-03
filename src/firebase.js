93a8146f26f34463ce1b2a1d58c1923678d88443
seijirom <seijiro.moriyama@gmail.com>
2023-09-03 19:36:22 +0900

    2nd init

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcWfAlcYTCGJOyQ5Ed2UWjAurnm27hmME",
+  authDomain: "my-succinct-app.firebaseapp.com",
+  projectId: "my-succinct-app",
+  storageBucket: "my-succinct-app.appspot.com",
+  messagingSenderId: "589916422682",
+  appId: "1:589916422682:web:ddaca5b47e6b19db1747a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
